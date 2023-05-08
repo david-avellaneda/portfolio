c6948d272db1ee2bcd2de11b61f0fcd31103cd98
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2023-05-08 12:24:47 -0500

    Studies component added to modal window

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,25 @@
 import styles from "./index.module.css";
 import hacker from "../../../public/about-me/hacker.png";
 import ImageUploader from "../ImageUploader";
+import Modal from "../Modal";
+import { useState } from "react";
+import Studies from "../Studies";
 
 const About = ({ t }) => {
   const { title, text, buttons, img, modalWindows } = t;
+  const { modal1, modal2 } = modalWindows;
+
+  const [isOpen, setIsOpen] = useState({ modal1: false, modal2: false });
+
+  const handleOpen = (id) => {
+    setIsOpen({ ...isOpen, [`modal${id + 1}`]: true });
+    document.body.classList.add("no-scroll");
+  };
+
+  const handleClose = (id) => {
+    setIsOpen({ ...isOpen, [`modal${id + 1}`]: false });
+    document.body.classList.remove("no-scroll");
+  };
 
   return (
     <section className={styles.container} id="about">
@@ -12,7 +28,7 @@ const About = ({ t }) => {
         <p>{text}</p>
         <div className={styles.buttons}>
           {buttons.map((button, id) => (
-            <button key={id}>
+            <button key={id} onClick={() => handleOpen(id)}>
               {button}
               <span></span>
             </button>
@@ -30,6 +46,9 @@ const About = ({ t }) => {
         </div>
         <p>{`"${img.text}"`}</p>
       </div>
+      <Modal isOpen={isOpen.modal1} handleClose={handleClose} modal={0}>
+        <Studies isOpen={isOpen.modal1} t={modal1} />
+      </Modal>
     </section>
   );
 };
