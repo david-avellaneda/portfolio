d23c0984cb9e0310514eea63b9576b5a20a9f261
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2023-05-08 11:34:34 -0500

    Dynamic header tags according to language

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import SplashScreen from "@/components/SplashScreen";
 import en from "@/locale/en";
 import es from "@/locale/es";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -19,6 +20,13 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <title>David Avellaneda | Front end Developer</title>
+        <meta name="description" content={t.head.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+        <meta name="author" content="David Santiago Avellaneda Montero" />
+      </Head>
       <SplashScreen />
       <Header
         theme={theme}
