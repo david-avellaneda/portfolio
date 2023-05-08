b0030291e3bbb578fca0d13a2050c900427b337a
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2023-05-08 10:47:46 -0500

    Adding font Baloo Thambi 2

---
+++ src/font/index.js
@@ -0,0 +1,9 @@
+import { Baloo_Thambi_2 } from "next/font/google";
+
+const balooThambi2 = Baloo_Thambi_2({
+  subsets: ["latin"],
+  variable: "--font-baloo_thambi",
+  weight: ["400", "500", "600", "700", "800"],
+});
+
+export default balooThambi2;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import balooThambi2 from "@/font";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${balooThambi2.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
